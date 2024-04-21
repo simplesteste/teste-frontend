4d26b7e0c57ba9b9c05d092b289f655c9f7f0416
danguya <daniel.yava16@gmail.com>
2024-04-21 18:29:23 +0100

    ttt

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,26 +1,26 @@
 import { Routes, Route } from 'react-router-dom'
 import { lazy, Suspense } from 'react'
 import Loader from '../components/Loader'
+import Login from '../pages/login'
+import Register from '../pages/register'
 
 const PrivateRoute = lazy(() => import('./private'))
-const Login = lazy(() => import('../pages/login'))
-const Register = lazy(() => import('../pages/register'))
 const NotFound = lazy(() => import('../pages/notfound'))
 const Profile = lazy(() => import('../pages/profile'))
 
 export default function MyRoutes() {
   return (
     <>
-      <Suspense fallback={<Loader />}>
-        <Routes>
+      <Routes>
+        <Suspense fallback={<Loader />}>
           <Route path="/" element={<PrivateRoute />}>
             <Route path="/" element={<Profile />} />
           </Route>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
           <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Suspense>
+        </Suspense>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   )
 }
